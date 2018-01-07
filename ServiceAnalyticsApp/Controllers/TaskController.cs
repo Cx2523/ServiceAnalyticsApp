6fe86e87052668be1482b59139159f44b001dc9c
Chris <Chris@PCONAMAC>
2018-01-07 09:58:35 -0700

    First Commit. Home Page, Form for Tasks, Tasks MVC

---
+++ ServiceAnalyticsApp/Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//models
+using ServiceAnalyticsApp.Models;
+
+namespace ServiceAnalyticsApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        // GET: Task
+        public ActionResult TaskManagement()
+        {
+            TaskModel Task = new TaskModel();
+            return View(Task);
+        }
+
+        [HttpPost]
+        public ActionResult TaskManagment()
+        {
+            return View();
+        }
+    }
+}
